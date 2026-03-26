4d09b1189ae9690d2128c71325c46e4b5097837b
jmculaban <culabanjohnmichael@gmail.com>
2026-03-26 09:16:08 +0800

    initial tf setup

---
+++ locals.tf
@@ -0,0 +1,14 @@
+locals {
+  project = var.project
+  env = var.environment
+
+  rg_name = "rg-${local.project}-simulator-${local.env}"
+  app_name = "app-${local.project}-api-${local.env}"
+  plan_name = "asp-${local.project}-${local.env}"
+  frontend_name = "stapp-${local.project}-frontend-${local.env}"
+  postgres_name = "psql-${local.project}-${local.env}"
+  appi_name = "appi-${local.project}-${local.env}"
+
+  # Storage must be globally unique, lowercase, no dashes
+  storage_name = lower("st${local.project}simulator${local.env}")
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "project" {
+  default = "exam"
+}
+
+variable "environment" {
+  default = "dev"
+}
